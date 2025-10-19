467be05d2d5c39606af0fc569bcfba2009844eab
Aisha <aishamnoushad@gmail.com>
2025-10-19 13:10:01 +0400

    initial commit

---
+++ RAG-Chatbot-Service/src/main/java/org/cloudjune/ragchatbotservice/RagChatbotServiceApplication.java
@@ -0,0 +1,13 @@
+package org.cloudjune.ragchatbotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagChatbotServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagChatbotServiceApplication.class, args);
+    }
+
+}
---
+++ RAG-Chatbot-Service/src/test/java/org/cloudjune/ragchatbotservice/RagChatbotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cloudjune.ragchatbotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagChatbotServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
