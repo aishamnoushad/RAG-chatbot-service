508edcf9c8862b28029c5c8dec7c1fcfe39ce7f0
Aisha <aishamnoushad@gmail.com>
2025-10-21 21:49:14 +0400

    updated the compose file, added the dockerfile and open api configuartion added to use the api key in the header for every request

--- RAG-Chatbot-Service/src/main/java/org/cloudjune/ragchatbotservice/config/OpenAPIConfiguration.java
+++ RAG-Chatbot-Service/src/main/java/org/cloudjune/ragchatbotservice/config/OpenAPIConfiguration.java
@@ -1,17 +1,47 @@
 package org.cloudjune.ragchatbotservice.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@SecurityScheme(name = "basicAuth", type = SecuritySchemeType.HTTP, scheme = "basic")
-@OpenAPIDefinition(info = @Info(title = "RAG Chat service APIs", version = "v1"),
-        security = @SecurityRequirement(name = "basicAuth"))
-@Profile(value = "dev")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "RAG Chatbot Service APIs", 
+                version = "v1.0.0",
+                description = "REST APIs for RAG Chatbot Service with chat sessions and message management",
+                contact = @Contact(name = "CloudJune Team"),
+                license = @License(name = "MIT License")
+        )
+)
 public class OpenAPIConfiguration {
+
+    @Value("${app.api.key:your-secure-api-key-here}")
+    private String apiKey;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("ApiKeyAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("X-Api-Key")
+                                        .description("API Key for authentication. Use the value from app.api.key property.")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("ApiKeyAuth"));
+    }
+
+
 }
+
